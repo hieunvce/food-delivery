355cf9005cdcd221ce9778ea5a7d43c08ab89d77
Hieu Nguyen <hieunv.ce@gmail.com>
2019-05-26 20:49:20 +0700

    Add add new product

--- webapp/grabfood/src/app/restaurant/menu-add/menu-add.component.ts
+++ webapp/grabfood/src/app/restaurant/menu-add/menu-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../service/product.schema';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductapiService } from '../service/productapi.service';
 
 @Component({
   selector: 'app-menu-add',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-add.component.scss']
 })
 export class MenuAddComponent implements OnInit {
+  private restaurantId: string;
+  private product: Product;
+  private productForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private api: ProductapiService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.restaurantId = this.route.snapshot.params["id"];
+    this.productForm = this.formBuilder.group({
+      restaurantId: [this.restaurantId],
+      name: [null,Validators.required],
+      price: [null,Validators.required],
+      description: [null],
+      category:[null],
+      img: [null],
+    });
   }
 
+  onFormSubmit(form: NgForm){
+    this.api.addProduct(form).subscribe(
+      res=>{
+        this.product = res;
+        this.router.navigate([
+          "/restaurant",
+          this.product.restaurantId,
+          "menu"
+        ]);
+      },
+      err=>{
+        console.log(err);
+      }
+    );
+  }
 }
